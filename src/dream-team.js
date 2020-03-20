23413a958833eaed9cc89480d1480a174781b974
PavelChukashev <chukashov93@gmail.com>
2020-03-21 00:37:08 +0300

    feat: complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,26 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  
+  if (!team) return false
+  const strArray = [];
+  for (let i=0; i < team.length; i++){
+    
+    if (typeof(team[i])=='string'){
+      
+      strArray.push(team[i].trim().toString().toUpperCase());
+    }
+  }
+  
+  const sortedArray = strArray.sort();
+  
+
+  let teamName = '';
+
+  for (let j=0; j < sortedArray.length; j++){
+    
+    teamName = teamName + sortedArray[j].charAt(0);
+  }
+  
+
+  return teamName;
+  
 };
